b78dadc241117b738eae2726c2cc45cb8c3c494a
joelfreney@gmail.com <joelfreney@gmail.com>
2024-03-18 20:32:55 +1100

    remove

--- leagueController.js
+++
@@ -1,8 +0,0 @@
-function handleLeaguesRequest(req, res) {
-    const userId = req.params.userId;
-    knex('leagues').where({ player_id: userId }).then(leagues => {
-      res.json(leagues);
-    }).catch(err => {
-      // Handle error
-    });
-  }
